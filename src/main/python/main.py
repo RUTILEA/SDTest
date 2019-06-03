f21182f357924a4985da8c2e1add89212ea2d1bb
OSHIMA Yuichi <oshima@dryout.co.jp>
2019-06-03 22:59:17 +0900

    Integrate FBS-Sentry for tracking errors in the compiled executable.

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -2,11 +2,36 @@ from fbs_runtime.application_context import ApplicationContext
 from PyQt5.QtWidgets import QStyleFactory
 from view.startup import StartupWidget
 from model.camera_model import CameraModel
+from fbs_runtime.application_context import ApplicationContext, cached_property, is_frozen
+from fbs_runtime.excepthook.sentry import SentryExceptionHandler
 
 import sys
 import pathlib
 
 class AppContext(ApplicationContext):           # 1. Subclass ApplicationContext
+
+    @cached_property
+    def exception_handlers(self):
+        result = super().exception_handlers
+        if is_frozen():
+            result.append(self.sentry_exception_handler)
+        return result
+
+    @cached_property
+    def sentry_exception_handler(self):
+        return SentryExceptionHandler(
+            self.build_settings['sentry_dsn'],
+            self.build_settings['version'],
+            self.build_settings['environment'],
+            callback=self._on_sentry_init
+        )
+    def _on_sentry_init(self):
+        scope = self.sentry_exception_handler.scope
+        from fbs_runtime import platform
+        scope.set_extra('os', platform.name())
+        # TODO: エラー情報に紐付けるユーザ情報
+        # scope.user = {'id': 41, 'email': 'john@gmail.com'}
+
     def run(self):                             # 2. Implement run()
         """ start QtApplication """
 
