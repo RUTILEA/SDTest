29fdc0ce07f81b6c56089223af412ea94c55b501
a.sato <akshugares@gmail.com>
2019-06-12 03:48:10 +0900

    remove commentout

--- src/main/python/view/main_window.py
+++ src/main/python/view/main_window.py
@@ -35,10 +35,10 @@ class MainWindow(QMainWindow):
         self.msgBox = None
 
         self.inspection_mainwindow_size = QSize(780, 482)
-        self.optimization_mainwindow_size = QSize(864, 632)#888
+        self.optimization_mainwindow_size = QSize(864, 632)
         self.past_result_mainwindow_size = QSize(780, 512)
         self.inspection_widget_size = QSize(740, 385)
-        self.optimization_widget_size = QSize(840, 504)#864
+        self.optimization_widget_size = QSize(840, 504)
         self.past_result_widget_size = QSize(740, 385)
 
         self.setup_tool_bar()
