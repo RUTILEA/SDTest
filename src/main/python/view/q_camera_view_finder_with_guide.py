9c60f5d236caf332021e2a56730aa3f9cc80a4a0
Kyosuke Shibata <kyousuke413@gmail.com>
2019-06-12 20:30:04 +0900

    Update: guide

--- src/main/python/view/q_camera_view_finder_with_guide.py
+++ src/main/python/view/q_camera_view_finder_with_guide.py
@@ -1,5 +1,5 @@
 from PyQt5.QtMultimediaWidgets import QCameraViewfinder
-from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtGui import QPainter, QColor, QPen
 
 
 class QCameraViewFinderWithGuide(QCameraViewfinder):
@@ -12,8 +12,11 @@ class QCameraViewFinderWithGuide(QCameraViewfinder):
         super().paintEvent(event)
         if self.needs_guide and super().mediaObject():
             painter = QPainter(self)
-            painter.setPen(QColor('#3e3e3e'))
-            line_length = 60
+            LINE_LENGTH_RATE = 30/326
+            line_length = LINE_LENGTH_RATE * self.width()
+            pen = QPen(QColor('#ffa00e'))
+            pen.setWidth(4)
+            painter.setPen(pen)
             painter.drawLine(super().width()/2, super().height()/2,
                              super().width()/2 + line_length, super().height()/2)
             painter.drawLine(super().width() / 2, super().height() / 2,
