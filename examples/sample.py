e5e4103a3ec23fc905a1892a25b3e39b189a399c
a.sato <akshugares@gmail.com>
2019-06-20 01:36:39 +0900

    several tests for some works

--- examples/sample.py
+++ examples/sample.py
@@ -3,9 +3,13 @@ import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 import sys
+import numpy as np
 sys.path.append(os.path.join(os.path.dirname(__file__), '../src/main/python/module'))
 from novelty_detector import NoveltyDetector
-
+from scipy.stats import median_absolute_deviation as mad, median_test, mannwhitneyu as U_test
+from statistics import median, stdev, mean
+from math import sqrt
+import csv
 
 def execute_cmdline():
     parser = argparse.ArgumentParser()
@@ -14,7 +18,7 @@ def execute_cmdline():
                         help='''path to image directory like examples/testimages/campbelle.
                         Expect path/train/OK, path/test/OK, and path/test/NG exist''',
                         type=str)
-    
+
     parser.add_argument('-n','--nn',
                         nargs='?',
                         default='ResNet',
@@ -55,9 +59,9 @@ def execute_cmdline():
                             
     args = parser.parse_args()
 
-    train_path = os.path.join(args.path, 'train', 'OK')
-    testok_path = os.path.join(args.path, 'test', 'OK')
-    testng_path = os.path.join(args.path, 'test', 'NG')
+    train_path = os.path.join(args.path, 'train', 'trim')
+    testok_path = os.path.join(args.path, 'test', 'OKtrim')
+    testng_path = os.path.join(args.path, 'test', 'NGtrim')
 
     if not os.path.exists(train_path):
         print(train_path, 'does not exist')
@@ -81,6 +85,12 @@ def execute_cmdline():
     testng_features = model.extracting_model.predict(testng_imgs)
     testok_dists = model.clf.decision_function(testok_features)
     testng_dists = model.clf.decision_function(testng_features)
+
+    train_paths = model._get_paths_in_dir(train_path)
+    train_imgs = model._read_imgs(train_paths)
+    train_features = model.extracting_model.predict(train_imgs)
+    train_dists = model.clf.decision_function(train_features)
+
 
     # Count how many normal items are classified correctly
     thr = args.threshold
@@ -117,8 +127,9 @@ def execute_cmdline():
         
 
     plt.figure()
-    sns.distplot(testok_dists, kde=True, rug=True, label='TEST OK')
+    sns.distplot(train_dists, kde=True, rug=True, label='TRAIN OK')
     sns.distplot(testng_dists, kde=True, rug=True, label='TEST NG')
+    sns.distplot(testok_dists, kde=True, rug=True, label='TEST OK')
     plt.title('Novelty detection on {}th layer on {} and {}'.format(
         args.layer, args.nn, args.detector)
     )
@@ -129,6 +140,76 @@ def execute_cmdline():
         plt.savefig(args.file_name)
     else:
         plt.show()
+
+    # T statistics
+
+    n = len(train_dists)
+    m = len(testok_dists)
+
+    std_train = stdev(train_dists)
+    std_testok = stdev(testok_dists)
+    nm = sqrt(1/n+1/m)
+    s = sqrt(std_train/n+std_testok/m)
+    T = abs(mean(train_dists)-mean(testok_dists))/s
+    F = std_testok/std_train
+    trT = abs(mean(train_dists)-mean(testok_dists))/(nm*std_train)
+    teT = abs(mean(train_dists)-mean(testok_dists))/(nm*std_testok)
+
+    print('')
+    # print('mean_train:'+str(mean(train_dists)))
+    print('std_train:'+str(std_train))
+    # print('mean_testok:'+str(mean(testok_dists)))
+    print('std_testok:'+str(std_testok))
+    print('diff_mean:'+str(abs(mean(train_dists) - mean(testok_dists))))
+    print('nm:'+str(nm))
+    print('s:'+str(s))
+    print('T:'+str(T))
+    print('trT:'+str(trT))
+    print('teT:'+str(teT))
+    print('F:'+str(F))
+
+
+    # MAD statistics
+
+    mad_train = mad(train_dists)
+    mad_testok = mad(testok_dists)
+    s = sqrt(mad_train/n+mad_testok/m)
+    med_T = abs(median(train_dists)-median(testok_dists))/s
+    med_trT = abs(median(train_dists)-median(testok_dists))/(nm*mad_train)
+    med_teT = abs(median(train_dists)-median(testok_dists))/(nm*mad_testok)
+    med_F = mad_testok/mad_train
+
+    print('')
+    # print('median_train:'+str(median(train_dists)))
+    print('mad_train:'+str(mad_train))
+    # print('median_testok:'+str(median(testok_dists)))
+    print('mad_testok:'+str(mad_testok))
+    print('diff_median:'+str(abs(median(train_dists) - median(testok_dists))))
+    print('nm:'+str(nm))
+    print('s:'+str(s))
+    print('med_T:'+str(med_T))
+    print('med_trT:'+str(med_trT))
+    print('med_teT:'+str(med_teT))
+    print('med_F:'+str(med_F))
+
+
+    # U test
+    print('')
+    print('U test')
+    u_p = U_test(train_dists, testok_dists)[1]
+    print('u_p:'+str(u_p))
+
+
+    # median test
+    print('')
+    print('median test')
+    med_p = median_test(train_dists, testok_dists)[1]
+    print('med_p:'+str(med_p))
+
+    with open('examples/hist/statistics.csv', 'a') as f:
+        writer = csv.writer(f)
+        # writer.writerow(['work', 'how near dists are?', 'T', 'trT', 'teT', 'F', 'med_T', 'med_trT', 'med_teT', 'med_F', 'U_test_p', 'median_test_p'])
+        writer.writerow([os.path.basename(os.path.dirname(args.path)), None, T, trT, teT, F, med_T, med_trT, med_teT, med_F, u_p, med_p])
 
 
 if __name__ == '__main__':
