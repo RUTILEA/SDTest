c1b2ae930c0201944ed4c09624a255f3378afdd2
Kyosuke Shibata <kyousuke413@gmail.com>
2019-07-03 17:09:00 +0900

    tmp

--- src/main/python/model/camera_model.py
+++ src/main/python/model/camera_model.py
@@ -1,7 +1,10 @@
+from module.pyuvc import uvc
 from PyQt5.QtMultimedia import QCamera, QCameraImageCapture, QImageEncoderSettings, QCameraInfo
 from datetime import datetime
-from typing import Dict
-from PyQt5.QtCore import pyqtSignal, QObject
+import threading
+from PyQt5.QtCore import pyqtSignal, QObject, QTimer, QSize
+from PyQt5.QtGui import QImage
+import queue
 import cv2
 
 
@@ -9,6 +12,7 @@ class CameraModel(QObject):
     __default_instance = None
     """This class provides QCamObjects"""
     image_saved = pyqtSignal(str)
+    get_video_image_by_timer = pyqtSignal()
 
     @classmethod
     def default(cls):
@@ -19,16 +23,25 @@ class CameraModel(QObject):
 
     @classmethod
     def get_available_camera_names(cls) -> list:
-        return [QCamera.deviceDescription(device_obj) for device_obj in QCamera.availableDevices()]
+        # print(uvc.device_list())
+        # uvc.Capture(uvc.device_list()[0]["uid"])
+        return [device_info['name'] for device_info in uvc.device_list()]
+        # return [QCamera.deviceDescription(device_obj) for device_obj in QCamera.availableDevices()]
 
     def __init__(self):
         super().__init__()
-        self.cams: Dict[str, QCamera] = {}
-        self.__cam_image_captures: Dict[str, QCameraImageCapture] = {}
+        self.cams = {}
+        # self.__cam_image_captures: Dict[str, QCameraImageCapture] = {}
+        self.__queues = {}
+        self.__old_device_list = []
         self.__fetch_cam()
-        self.selected_cam_names = []
+        self.selected_cam_names = [self.get_available_camera_names()[0]]
+        self.images = {}
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.get_playing_qimage)
+        self.timer.start(1)
         try:
-            default_camera_name = QCameraInfo.defaultCamera().description()
+            default_camera_name = self.get_available_camera_names()[0]
             if default_camera_name is not '':
                 self.selected_cam_names.append(default_camera_name)
         # TODO: Find correct error by using the computer which has no default camera
@@ -36,32 +49,64 @@ class CameraModel(QObject):
             print('No default camera')
 
     def __fetch_cam(self):
-        for device_obj in QCamera.availableDevices():
-            cam_name = QCamera.deviceDescription(device_obj)
+        for i, device in enumerate(uvc.device_list()):
+            if device['name'] not in self.__old_device_list:
+                self.__queues[device['name']] = queue.Queue()
+                cam = uvc.Capture(device["uid"])
+                self.cams[device['name']] = cam
+                print(cam.avaible_modes)
+                controller_dict = dict([(c.display_name, c) for c in cam.controls])
+                print(controller_dict.keys())
+                if 'Auto Focus' in controller_dict:
+                    controller_dict['Auto Focus'].value = 0
+                print(device['name'])
+                uvc_thread = threading.Thread(target=self.grab_uvc, args=(device['name'], cam))
+                uvc_thread.start()
+                self.__old_device_list.append(device['name'])
 
-            cam = QCamera(device_obj)
-            self.cams[str(cam_name)] = cam
+    def grab_uvc(self, device_name, uvc_capture):
+        width, height, fps = uvc_capture.avaible_modes[2]
+        print((width, height, fps))
+        uvc_capture.frame_mode = (width, height, fps)
+        while True:
+            frame = uvc_capture.get_frame_robust()
+            self.images[device_name] = frame.img
 
-            cam_image_capture = QCameraImageCapture(cam)
-            cam_image_capture.setEncodingSettings(QImageEncoderSettings())
-            cam_image_capture.captureDestination()
-            cam_image_capture.imageSaved.connect(self.on_image_saved)
-            self.__cam_image_captures[str(cam_name)] = cam_image_capture
-            cam.statusChanged.connect(self.set_resolution)
+    # def img_converter(self, image):
+    #     # scale_w = float(img_width) / float(img_width)
+    #     # scale_h = float(img_height) / float(img_height)
+    #     # scale = min([scale_w, scale_h])
+    #     #
+    #     #
+    #     # image = cv2.resize(image, None, fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
+    #     image =
+    #     return image
+
+    def get_playing_qimage(self):
+        if self.selected_cam_names[0] not in self.images:
+            print('a')
+            return
+        else:
+            # print(self.selected_cam_names[0])
+            image = self.images[str(self.selected_cam_names[0])]
+            cv2.imshow("img", image)
+            self.get_video_image_by_timer.emit(image)
+            # cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            # height, width, bpc = image.shape
+            # bpl = bpc * width
+            # q_image = QImage(image.data, width, height, bpl, QImage.Format_RGB888)
+            # self.get_video_image_by_timer.emit(q_image)
 
     def capture(self, directory: str):
-        for i, cam_name in enumerate(self.selected_cam_names):
-            cap: QCameraImageCapture = self.__cam_image_captures[cam_name]
-            cam: QCamera = self.cams[cam_name]
-            # TODO: manage image name format (e.x. use Dataset.generate_image_path())
-            timestamp = str(datetime.now().isoformat()).replace(':', '-')
-            file_name = f'camera_{i}_{timestamp}.jpg'
-            image_path = directory + '/' + file_name
-            cam.searchAndLock()
-            cap.capture(image_path)
-            cam.unlock()
+        # TODO: manage image name format (e.x. use Dataset.generate_image_path())
+        timestamp = str(datetime.now().isoformat()).replace(':', '-')
+        file_name = f'camera_{0}_{timestamp}.jpg'
+        image_path = directory + '/' + file_name
+        if self.images[self.selected_cam_names[0]] is not None:
+            cv2.imwrite(image_path, self.images[self.selected_cam_names[0]])
+            self.on_image_saved(image_path)
 
-    def on_image_saved(self, id, image_path):
+    def on_image_saved(self, image_path):
         RESIZED_WIDTH = 640
         img = cv2.imread(image_path)
         img_height, img_width, _ = img.shape
@@ -70,33 +115,16 @@ class CameraModel(QObject):
         cv2.imwrite(image_path, img)
         self.image_saved.emit(image_path)
 
-    def start(self):
-        for key, val in self.cams.items():
-            val.start()
-
-    def stop(self):
-        for key, val in self.cams.items():
-            val.stop()
-
-    def set_resolution(self):
-        for cam_name, cam in self.cams.items():
-            image_settings = QImageEncoderSettings()
-            for size in cam.supportedViewfinderResolutions():
-                if 600 <= size.width() <= 700:
-                    image_settings.setResolution(size.width(), size.height())
-            self.__cam_image_captures[cam_name].setEncodingSettings(image_settings)
-
     def set_selected_camera_to_view_finder(self, view_finder_obj):
         # TODO:Handle many cameras on version 2.0 or more
         if len(self.selected_cam_names) > 0:
             self.__fetch_cam()
-            cam: QCamera = self.cams[self.selected_cam_names[0]]
-            cam.setViewfinder(view_finder_obj)
-            self.start()
+            # cam: QCamera = self.cams[self.selected_cam_names[0]]
+            # cam.setViewfinder(view_finder_obj)
 
     def connect_view_finders_with_all_cameras(self, selectable_camera_view_list):
         self.__fetch_cam()
-        for selectable_camera_view in selectable_camera_view_list:
-            selectable_camera_view.set_q_cam(self.cams[selectable_camera_view.get_cam_name()])
-        self.start()
+        # for selectable_camera_view in selectable_camera_view_list:
+        #     selectable_camera_view.set_q_cam(self.cams[selectable_camera_view.get_cam_name()])
+
 
