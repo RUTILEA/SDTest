85f3e285f75c74aa8ce5ec479a96079f8033c0ee
Kyosuke Shibata <kyousuke413@gmail.com>
2019-07-03 17:14:06 +0900

    tmp

--- src/main/python/view/q_camera_view_finder_with_guide.py
+++ src/main/python/view/q_camera_view_finder_with_guide.py
@@ -1,27 +1,40 @@
-from PyQt5.QtMultimediaWidgets import QCameraViewfinder
 from PyQt5.QtGui import QPainter, QColor, QPen
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import QPoint
+from model.camera_model import CameraModel
 
 
-class QCameraViewFinderWithGuide(QCameraViewfinder):
-
+class QCameraViewFinderWithGuide(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.needs_guide = True
+        self.image = None
+        CameraModel.default().get_video_image_by_timer.connect(self.set_image)
+
+    def set_image(self, image):
+        self.image = image
+        # print(self.image)
+        self.setMinimumSize(image.size())
+        self.update()
 
     def paintEvent(self, event):
-        super().paintEvent(event)
-        if self.needs_guide and super().mediaObject():
-            painter = QPainter(self)
-            LINE_LENGTH_RATE = 30/326
-            line_length = LINE_LENGTH_RATE * self.width()
-            pen = QPen(QColor('#ffa00e'))
-            pen.setWidth(4)
-            painter.setPen(pen)
-            painter.drawLine(super().width()/2, super().height()/2,
-                             super().width()/2 + line_length, super().height()/2)
-            painter.drawLine(super().width() / 2, super().height() / 2,
-                             super().width() / 2 - line_length, super().height() / 2)
-            painter.drawLine(super().width() / 2, super().height() / 2,
-                             super().width() / 2, super().height() / 2 + line_length)
-            painter.drawLine(super().width() / 2, super().height() / 2,
-                             super().width() / 2, super().height() / 2 - line_length)
+        qp = QPainter()
+        qp.begin(self)
+        if self.image:
+            qp.drawImage(QPoint(0, 0), self.image)
+        qp.end()
+        # if self.needs_guide:
+        #     painter = QPainter(self)
+        #     LINE_LENGTH_RATE = 30/326
+        #     line_length = LINE_LENGTH_RATE * self.width()
+        #     pen = QPen(QColor('#ffa00e'))
+        #     pen.setWidth(4)
+        #     painter.setPen(pen)
+        #     painter.drawLine(super().width()/2, super().height()/2,
+        #                      super().width()/2 + line_length, super().height()/2)
+        #     painter.drawLine(super().width() / 2, super().height() / 2,
+        #                      super().width() / 2 - line_length, super().height() / 2)
+        #     painter.drawLine(super().width() / 2, super().height() / 2,
+        #                      super().width() / 2, super().height() / 2 + line_length)
+        #     painter.drawLine(super().width() / 2, super().height() / 2,
+        #                      super().width() / 2, super().height() / 2 - line_length)
