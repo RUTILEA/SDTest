e22003c216cbe388093b841e084895ca58c6e7e6
a.sato <akshugares@gmail.com>
2019-07-20 07:42:16 +0900

    refactoring ui files

--- src/main/python/view/ui/inspection.py
+++ src/main/python/view/ui/inspection.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/Users/satoakitaka/Documents/rutilea/OSS/SDTest/src/main/python/view/ui/inspection.ui'
+# Form implementation generated from reading ui file 'inspection.ui'
 #
 # Created by: PyQt5 UI code generator 5.12.2
 #
